d652a560e3be3f006ac9a1e264d736408efda5c6
sdeepti <sdeepti@vt.edu>
2020-05-29 17:02:33 -0400

    SignInCommit

--- app.py
+++ app.py
@@ -9,16 +9,59 @@ db = SQLAlchemy(app)
 
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(200), nullable=False)
-
+    username = db.Column(db.String(200),nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+    name = db.Column(db.String(200), nullable=False)
+    email = db.Column(db.String(200), nullable=False)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
     
     def __repr__(self):
         return '<Task %r>' % self.id
 
+@app.route('/')
+def home():
+    return render_template("index.html")
 
-@app.route('/', methods=['POST','GET'])
-def index():
+#this is our sign in page!
+@app.route('/signin', methods=['POST', 'GET'])
+def signin():
+    username = request.get['user']
+    password = request.get['pass']
+
+    return render_template("signin.html")
+
+@app.route('/showSignUp')
+def showSignUp():
+    return render_template("signup.html")
+
+#this is our sign up page!
+@app.route('/signup', methods=['POST', 'GET'])
+def signup():
+    if request.method == 'POST':    
+        username = request.form['user']
+        password = request.form['pass']
+        name = request.form['name']
+        email = request.form['email']
+        print("cheesecake")
+        print(username)
+        #creating a database for new user
+        new_user = Todo(username=username, password=password, name=name, email=email)
+
+        try:
+            db.session.add(new_user)
+            db.session.commit()
+            return redirect(url_for('signin'))
+
+        except:
+            return ("There was an issue adding this user plus YOU SUCK ")
+
+  
+@app.route('/dashboard')
+def dashboard():
+    return ""
+
+@app.route('/enter_meal', methods=['POST','GET'])
+def enter_meal():
 
     if request.method == 'POST':
         task_content = request.form['content']
@@ -33,7 +76,30 @@ def index():
     
     else:
         tasks = Todo.query.order_by(Todo.date_created).all()
-        return render_template('index.html', tasks=tasks)
+        return render_template('enter_meal.html', meal=tasks)
+
+
+
+@app.route('/enter_water', methods=['POST','GET'])
+def enter_water():
+    return ""
+
+@app.route('/view_progress', methods=['POST','GET'])
+def view_progress():
+    return ""
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 @app.route('/delete/<int:id>')
@@ -66,6 +132,7 @@ def update(id):
 
     else:
         return render_template('update.html', task=task)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
